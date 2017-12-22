0bb8e537bcc419ec28a92630c86a9b7d4a0123bd
Graeme Lockley <graemel@no9.co.za>
2017-12-22 06:25:18 +0200

    feature: read the port from process.env

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const Express = require("express");
 
 const app = Express();
 
+const PORT =
+    process.env.PORT || 5000;
+
+
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}`));
