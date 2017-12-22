8cb5c9d538e19f0a660d981eccf14bb99471b3f9
Graeme Lockley <graemel@no9.co.za>
2017-12-22 06:07:58 +0200

    feature: create simple Hello World base

---
+++ index.js
@@ -0,0 +1,7 @@
+const Express = require("express");
+
+const app = Express();
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
