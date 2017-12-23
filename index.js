bd41da1227a259b63f3d201903f416fe5831c4e5
Graeme Lockley <graemel@no9.co.za>
2017-12-23 08:38:34 +0200

    refactor: change the shape to sle style

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
-const Express = require("express");
-const QueryString = require("querystring");
+require("sle");
 
-const app = Express();
+$importAll([
+    "express",
+    "querystring",
+    "core:Native.Data.Array:1.2.0"
+]).then($imports => {
+    const Express = $imports[0];
+    const QueryString = $imports[1];
 
-const PORT =
-    process.env.PORT || 5000;
+    const app = Express();
+
+    const PORT =
+        process.env.PORT || 5000;
 
 
-app.get('/', (req, res) => res.send('Hello World!'));
+    app.get('/', (req, res) => res.send('Hello World!'));
 
 
-app.put("/process", (req, res) => {
-    let body = "";
-    req.on('data', data => {
-        body += data;
+    app.put("/process", (req, res) => {
+        let body = "";
+        req.on('data', data => {
+            body += data;
+        });
+        req.on('end', () => {
+            console.log(`Process Put: ${body}`);
+            res.writeHead(200, "OK", {'Content-Type': 'text.plain'});
+            res.end();
+        });
     });
-    req.on('end', () => {
-        console.log(`Process Put: ${body}`);
-        res.writeHead(200, "OK", {'Content-Type': 'text.plain'});
-        res.end();
-    });
+
+
+    app.listen(PORT, () => console.log(`Example app listening on port ${PORT}`));
 });
-
-
-app.listen(PORT, () => console.log(`Example app listening on port ${PORT}`));
