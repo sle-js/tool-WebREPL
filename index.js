28d0a255aa576a784ab6e99656b68fa29bda9283
Graeme Lockley <graemel@no9.co.za>
2017-12-23 08:35:00 +0200

    feature: add a put for creating a new process

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Express = require("express");
+const QueryString = require("querystring");
 
 const app = Express();
 
@@ -7,5 +8,19 @@ const PORT =
 
 
 app.get('/', (req, res) => res.send('Hello World!'));
+
+
+app.put("/process", (req, res) => {
+    let body = "";
+    req.on('data', data => {
+        body += data;
+    });
+    req.on('end', () => {
+        console.log(`Process Put: ${body}`);
+        res.writeHead(200, "OK", {'Content-Type': 'text.plain'});
+        res.end();
+    });
+});
+
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}`));
