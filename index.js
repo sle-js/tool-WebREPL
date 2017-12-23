70ddf048ca33651713d486256c7c0d28286bdc82
Graeme Lockley <graemel@no9.co.za>
2017-12-23 16:43:24 +0200

    refactor: pull the loading of the body out into a Promise

--- index.js
+++ index.js
@@ -14,20 +14,36 @@ $importAll([
         process.env.PORT || 5000;
 
 
+    const loadBody = (req, res) =>
+        new Promise((resolve, reject) => {
+            let body = "";
+            req.on('error', error => {
+                reject(error);
+            });
+            req.on('data', data => {
+                body += data;
+            });
+            req.on('end', () => {
+                resolve(body);
+            });
+        });
+
+
     app.get('/', (req, res) => res.send('Hello World!'));
 
 
-    app.put("/process", (req, res) => {
-        let body = "";
-        req.on('data', data => {
-            body += data;
-        });
-        req.on('end', () => {
-            console.log(`Process Put: ${body}`);
-            res.writeHead(200, "OK", {'Content-Type': 'text.plain'});
-            res.end();
-        });
-    });
+    app.put("/process", (req, res) =>
+        loadBody(req, res)
+            .then(body => {
+                console.log(`Process Put: ${body}`);
+                res.writeHead(200, "OK", {'Content-Type': 'text/plain'});
+                res.end();
+            })
+            .catch(err => {
+                console.log(`Error: ${err}`);
+                res.writeHead(500, "" + err.status, {'Content-Type': 'application/json'});
+                res.end(JSON.stringify(err, null, 2));
+            }));
 
 
     app.listen(PORT, () => console.log(`Example app listening on port ${PORT}`));
